096e561a958da9b59256d5fec34a53df11dab0e1
Charlie Phillips <charlie_l_phillips@proton.me>
2025-01-17 17:01:36 +0000

    Update main.rs

--- source/services/src/main.rs
+++ source/services/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    //https://rust-cli.github.io/book/tutorial/cli-args.html
     let arg1 = std::env::args().nth(1).expect("no arg1 given");
     let arg2 = std::env::args().nth(2).expect("no arg2 given");
 
