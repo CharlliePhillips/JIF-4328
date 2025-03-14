89dffcbd52bda4b49e72be95b9988434829c5abd
DerputeR(WSL) <dekxy-developer@yahoo.com>
2025-03-14 18:04:24 -0400

    style: ran cargo fmt on services

--- source/services/src/main.rs
+++ source/services/src/main.rs
@@ -28,16 +28,17 @@ fn main() {
         .encode()
         .expect("Failed to encode command to byte buffer");
 
-    File::write(sm_fd, &cmd_bytes)
-            .expect("Failed to write command to service monitor");
+    File::write(sm_fd, &cmd_bytes).expect("Failed to write command to service monitor");
 
     // print_response(&cli.cmd, sm_fd);
     let response: Vec<u8> = get_response(sm_fd);
     if response.len() > 0 {
         // TODO: replace with TOML parsing and dynamically construct the correct printout
-        println!("{}", std::str::from_utf8(&response)
-            .expect("Error parsing response to UTF8")
-            .to_string()
+        println!(
+            "{}",
+            std::str::from_utf8(&response)
+                .expect("Error parsing response to UTF8")
+                .to_string()
         );
     }
 }
@@ -46,8 +47,7 @@ fn get_response(sm_fd: &mut File) -> Vec<u8> {
     let mut response = Vec::<u8>::new();
     loop {
         let mut buf = [0u8; 1024];
-        let size =
-            File::read(sm_fd, &mut buf).expect("Failed to read PIDs from service monitor");
+        let size = File::read(sm_fd, &mut buf).expect("Failed to read PIDs from service monitor");
         if size == 0 {
             break;
         }
